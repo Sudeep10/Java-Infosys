c6d35c6f1b3cdbcea0a8b58168cfca5b0c69a37d
Sudeep Shetty <shettysudeep1010@gmail.com>
2023-12-15 22:31:05 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,45 +1,14 @@
-public class Student {
+import java.util.Objects;
 
-    private int studentId;
+public class Student {
     private String name;
-    private Grade grade;
-    private int scholarshipAmount;
+    private int rollNumber;
     private int totalMarks;
 
-    public enum Grade {
-        A, B, C, D, E
-    }
-
-    public int getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
+    public Student(String name, int rollNumber, int totalMarks) {
         this.name = name;
-    }
-
-    public Grade getGrade() {
-        return grade;
-    }
-
-    public void setGrade(Grade grade) {
-        this.grade = grade;
-    }
-
-    public int getScholarshipAmount() {
-        return scholarshipAmount;
-    }
-
-    public void setScholarshipAmount(int scholarshipAmount) {
-        this.scholarshipAmount = scholarshipAmount;
+        this.rollNumber = rollNumber;
+        this.totalMarks = totalMarks;
     }
 
     public int getTotalMarks() {
@@ -50,47 +19,39 @@ public class Student {
         this.totalMarks = totalMarks;
     }
 
-    public void calculateGrade() {
-        switch (totalMarks) {
-            case 250:
-                grade = Grade.A;
-                break;
-            case 200:
-                if (totalMarks < 250) {
-                    grade = Grade.B;
-                }
-                break;
-            case 175:
-                if (totalMarks < 200) {
-                    grade = Grade.C;
-                }
-                break;
-            case 150:
-                if (totalMarks < 175) {
-                    grade = Grade.D;
-                }
-                break;
-            default:
-                grade = Grade.E;
-        }
+    public String getName() {
+        return name;
     }
 
-    public void calculateScholarshipAmount() {
-        switch (grade) {
-            case A:
-                scholarshipAmount = 5000;
-                break;
-            case B:
-                scholarshipAmount = 4000;
-                break;
-            case C:
-                scholarshipAmount = 3000;
-                break;
-            case D:
-                scholarshipAmount = 2000;
-                break;
-            default:
-                scholarshipAmount = 0;
-        }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    // add toString() method
+    @Override
+    public String toString() {
+        return "Student name = " + name + ", rollNumber = " + rollNumber;
+    }
+
+    // add appropriate method for avoiding duplicate Student
+    @Override
+    public int hashCode() {
+        return rollNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student s = (Student) o;
+        return rollNumber == s.rollNumber || Objects.equals(getName(), s.name);
     }
 }
